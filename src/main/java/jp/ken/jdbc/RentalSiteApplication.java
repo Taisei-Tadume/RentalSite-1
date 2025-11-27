7cdca562d7af98b50dfe2286727cac0cfe66a00f
yoshinaga-c <1000ai.227@gmail.com>
2025-11-27 10:17:00 +0900

    初コミット

---
+++ src/main/java/jp/ken/jdbc/RentalSiteApplication.java
@@ -0,0 +1,13 @@
+package jp.ken.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalSiteApplication.class, args);
+	}
+
+}
