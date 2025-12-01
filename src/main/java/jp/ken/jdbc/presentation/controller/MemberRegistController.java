2773b2d2dcc77886a12f27bfe8834a411d8921fc
ichiyasa-g-3 <ichiyasa-g-3@example.com>
2025-12-01 12:57:24 +0900

    コントローラークラスの改修

--- src/main/java/jp/ken/jdbc/presentation/controller/MemberRegistController.java
+++ src/main/java/jp/ken/jdbc/presentation/controller/MemberRegistController.java
@@ -1,13 +1,18 @@
 package jp.ken.jdbc.presentation.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jp.ken.jdbc.presentation.form.MemberForm;
 
 @Controller
 public class MemberRegistController {
 
     @GetMapping("/regist")
-    public String regist() {
-        return "newmemberregistration";
+    public String regist(Model model) {
+    	MemberForm memberForm = new MemberForm();
+    	model.addAttribute("memberForm", memberForm);
+    	return "newmemberregistration";
     }
 }
