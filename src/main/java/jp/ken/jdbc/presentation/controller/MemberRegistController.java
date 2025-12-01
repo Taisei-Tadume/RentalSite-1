de7cd2eada6d00b3aaf0b8625a9b171c371e3b21
ichiyasa-g-3 <ichiyasa-g-3@example.com>
2025-12-01 13:30:31 +0900

    マッピングするオブジェクトを指定

--- src/main/java/jp/ken/jdbc/presentation/controller/MemberRegistController.java
+++ src/main/java/jp/ken/jdbc/presentation/controller/MemberRegistController.java
@@ -4,15 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import jp.ken.jdbc.presentation.form.MemberForm;
+import jp.ken.jdbc.presentation.form.MemberRegistForm;
 
 @Controller
 public class MemberRegistController {
 
     @GetMapping("/regist")
     public String regist(Model model) {
-    	MemberForm memberForm = new MemberForm();
-    	model.addAttribute("memberForm", memberForm);
+    	MemberRegistForm memberregistForm = new MemberRegistForm();
+    	model.addAttribute("memberregistForm", memberregistForm);
     	return "newmemberregistration";
     }
 }
