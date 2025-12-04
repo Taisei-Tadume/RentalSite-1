a2995350a8663cb436a0c2ed3b327133365f7809
ichiyasa-g-3 <ichiyasa-g-3@example.com>
2025-12-04 15:29:59 +0900

    Entityの修正

--- src/main/java/jp/ken/jdbc/domain/entity/GoodsEntity.java
+++ src/main/java/jp/ken/jdbc/domain/entity/GoodsEntity.java
@@ -2,36 +2,82 @@ package jp.ken.jdbc.domain.entity;
 
 public class GoodsEntity {
 
+    /** 商品ID */
     private int goodsId;
-    private int productId;
-    private int stock;
-    private String status;
+
+    /** 商品名 */
+    private String goodsName;
+
+    /** カテゴリID */
+    private int categoryId;
+
+    /** ジャンルID */
+    private int genreId;
+
+    /** 在庫数量 */
+    private int quantity;
+
+    /** JANコード */
+    private String janCode;
+
+    /** 画像URL */
+    private String imageUrl;
+
+    // Getter / Setter
 
     public int getGoodsId() {
         return goodsId;
     }
+
     public void setGoodsId(int goodsId) {
         this.goodsId = goodsId;
     }
 
-    public int getProductId() {
-        return productId;
-    }
-    public void setProductId(int productId) {
-        this.productId = productId;
+    public String getGoodsName() {
+        return goodsName;
     }
 
-    public int getStock() {
-        return stock;
-    }
-    public void setStock(int stock) {
-        this.stock = stock;
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName;
     }
 
-    public String getStatus() {
-        return status;
+    public int getCategoryId() {
+        return categoryId;
     }
-    public void setStatus(String status) {
-        this.status = status;
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public int getGenreId() {
+        return genreId;
+    }
+
+    public void setGenreId(int genreId) {
+        this.genreId = genreId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getJanCode() {
+        return janCode;
+    }
+
+    public void setJanCode(String janCode) {
+        this.janCode = janCode;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 }
