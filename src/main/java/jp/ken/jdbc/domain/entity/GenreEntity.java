747694339f3ac986f1f5e65d7b3414ee53714bc4
yoshinaga-c <1000ai.227@gmail.com>
2025-12-04 15:13:33 +0900

    entity変更

--- src/main/java/jp/ken/jdbc/domain/entity/GenreEntity.java
+++ src/main/java/jp/ken/jdbc/domain/entity/GenreEntity.java
@@ -2,18 +2,12 @@ package jp.ken.jdbc.domain.entity;
 
 public class GenreEntity {
 
-	/** ジャンルID */
     private int genreId;
-
-    /** ジャンル名 */
     private String genreName;
-
-    // Getter / Setter
 
     public int getGenreId() {
         return genreId;
     }
-
     public void setGenreId(int genreId) {
         this.genreId = genreId;
     }
@@ -21,7 +15,6 @@ public class GenreEntity {
     public String getGenreName() {
         return genreName;
     }
-
     public void setGenreName(String genreName) {
         this.genreName = genreName;
     }
--- src/main/java/jp/ken/jdbc/domain/entity/MemberEntity.java
+++ src/main/java/jp/ken/jdbc/domain/entity/MemberEntity.java
@@ -2,31 +2,53 @@ package jp.ken.jdbc.domain.entity;
 
 public class MemberEntity {
 
-    private Integer memberId;
-    private String name;
+    private int userId;
+    private String userName;
+    private String zipcode;
+    private String address;
+    private String phone;
     private String email;
     private String password;
+    private int adminFlag;
 
-    public Integer getMemberId() {
-        return memberId;
+    public int getUserId() {
+        return userId;
+    }
+    public void setUserId(int userId) {
+        this.userId = userId;
     }
 
-    public void setMemberId(Integer memberId) {
-        this.memberId = memberId;
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
-    public String getName() {
-        return name;
+    public String getZipcode() {
+        return zipcode;
+    }
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
@@ -34,8 +56,14 @@ public class MemberEntity {
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public int getAdminFlag() {
+        return adminFlag;
+    }
+    public void setAdminFlag(int adminFlag) {
+        this.adminFlag = adminFlag;
     }
 }
